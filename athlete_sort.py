2ca30859fa615d16606d84a29dd5d65d44b16ab0
nayak <unpandu104@gmail.com>
2019-02-23 00:52:05 +0530

    abc

--- athlete_sort.py
+++ athlete_sort.py
@@ -1,6 +1,6 @@
-k=1
-arr=[[19,8,7],[9,8,7],[4,10,6]]
-sorted_arr=sorted(arr,key=lambda x:x[k],reverse=True)
+k = 1
+arr = [[19, 8, 7], [9, 8, 7], [4, 10, 6]]
+sorted_arr = sorted(arr, key=lambda x: x[k], reverse=True)
 print(arr)
 print(sorted_arr)
 for x in sorted_arr:
